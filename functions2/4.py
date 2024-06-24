2d4114ab9654bcb9719f30d5256a2bcf5b9f167f
wayneushe <wayneushe@gmail.com>
2024-06-24 12:10:27 +0600

    Add files via upload

---
+++ functions2/4.py
@@ -0,0 +1,95 @@
+movies =[
+{
+"name": "usual suspects",
+"imdb": 7.0,
+"category":  "Thriller",
+
+},
+{
+"name": "Hitman",
+"imdb": 6.3,
+"category": "Action"
+},
+{
+"name": "Dark Knight",
+"imdb": 9.0,
+"category": "Adventure"
+},
+{
+"name": "The Help",
+"imdb": 8.0,
+"category": "Drama"
+},
+{
+"name": "The Choice",
+"imdb": 6.2,
+"category": "Romance"
+},
+{
+"name": "Colonia",
+"imdb": 7.4,
+"category": "Romance"
+},
+{
+"name": "Love",
+"imdb": 6.0,
+"category": "Romance"
+},
+{
+"name": "Bride Wars",
+"imdb": 5.4,
+"category": "Romance"
+},
+{
+"name": "AlphaJet",
+"imdb": 3.2,
+"category": "War"
+},
+{
+"name": "Ringing Crime",
+"imdb": 4.0,
+"category": "Crime"
+},
+{
+"name": "Joking muck",
+"imdb": 7.2,
+"category": "Comedy"
+},
+{
+"name": "What is the name",
+"imdb": 9.2,
+"category": "Suspense"
+},
+{
+"name": "Detective",
+"imdb": 7.0,
+"category": "Suspense"
+},
+{
+"name": "Exam",
+"imdb": 4.2,
+"category": "Thriller"
+},
+{
+"name": "We Two",
+"imdb": 7.2,
+"category": "Romance"
+}
+]
+
+"""def computed_average():
+    sum =0
+    count = len(movies)
+    for m in movies:
+        sum += m["imdb"]
+        return sum / count
+
+print(round(computed_average(), 2))"""
+
+def average_imdb(movies):
+    cnt =0
+    for x in movies :
+        cnt +=x['imdb']
+    cnt = cnt / len(movies)
+    return round(cnt,2)
+print("Average IMDB:",average_imdb(movies))    
